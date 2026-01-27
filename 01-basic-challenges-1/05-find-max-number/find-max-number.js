54cb60ab0f897ccf8fdbc85f9d3345955879bf01
numoonchld <numoonchld@gmail.com>
2026-01-27 19:50:27 +0530

    find max number

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,10 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let max = arr[0]
+
+  for (const num of arr) if (num > max) max = num
+
+  return max
+
+}
 
 module.exports = findMaxNumber;
