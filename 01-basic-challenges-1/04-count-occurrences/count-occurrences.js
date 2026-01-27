75b86382f8e1ca554808a8c5752b3e57ec2ab009
fc311 <formcollect311@gmail.com>
2026-01-27 19:26:42 +0530

    counting number of occurences of a character

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,20 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  // const charDictionary = {}
+
+  // for (const letter of str) {
+  //   if charDictionary[letter] {
+  //     charDictionary[letter]++
+  //   } else charDictionary[letter] = 1
+  // }
+
+  // return charDictionary[char]
+
+  let count = 0;
+
+  for (const letter of str) if (letter === char) count++;
+
+  return count;
+
+}
 
 module.exports = countOccurrences;
