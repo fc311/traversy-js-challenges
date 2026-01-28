4c94fba1ede175a06f4bfeae36a4be93c6967b32
fc311 <formcollect311@gmail.com>
2026-01-28 17:58:12 +0530

    reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,12 @@
-function reverseString() {}
+function reverseString(str) {
+
+  const returnStr = []
+
+  for (let counter = str.length - 1; counter >= 0; counter--) {
+    returnStr.push(str[counter])
+  }
+
+  return returnStr.join("")
+}
 
 module.exports = reverseString;
