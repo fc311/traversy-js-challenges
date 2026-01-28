17199ad77ac6a2e66deae0e234510fcb6a0c2935
fc311 <formcollect311@gmail.com>
2026-01-28 17:49:47 +0530

    title case convertor

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,16 @@
-function titleCase() {}
+function titleCase(str) {
+  const words = str.toLowerCase().split(" ")
+
+  let returnSentence = [];
+
+  for (const word of words) {
+    const letters = word.split("")
+    letters[0] = letters[0].toUpperCase()
+
+    returnSentence.push(letters.join(""))
+  }
+
+  return returnSentence.join(" ")
+}
 
 module.exports = titleCase;
