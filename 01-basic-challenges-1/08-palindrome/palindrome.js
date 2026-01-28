fc8b278e2dfc5f7edb96f54e6244cb3590684556
fc311 <formcollect311@gmail.com>
2026-01-28 18:46:24 +0530

    count vowels
    also clean up palindrome console.log

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -2,7 +2,6 @@ function isPalindrome(str) {
   const cleanedUpStr = str.toLowerCase().replace(/[^a-z0-9]/g, "")
   const backward = cleanedUpStr.split("").reverse().join("")
 
-  console.log(cleanedUpStr, backward)
   return cleanedUpStr === backward
 }
 
--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,14 @@
-function countVowels() {}
+function countVowels(str) {
+  let count = 0;
+
+  for (const letter of str) {
+    if (["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"
+    ].includes(letter)) count++
+  }
+
+  return count
+}
+
+
 
 module.exports = countVowels;
