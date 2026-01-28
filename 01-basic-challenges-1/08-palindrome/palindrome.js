e9416d9ffd8ddcef4f85bd938b9303f7fa714844
fc311 <formcollect311@gmail.com>
2026-01-28 18:40:13 +0530

    is palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,9 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const cleanedUpStr = str.toLowerCase().replace(/[^a-z0-9]/g, "")
+  const backward = cleanedUpStr.split("").reverse().join("")
+
+  console.log(cleanedUpStr, backward)
+  return cleanedUpStr === backward
+}
 
 module.exports = isPalindrome;
