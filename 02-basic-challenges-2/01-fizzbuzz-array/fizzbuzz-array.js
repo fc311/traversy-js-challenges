3d192d5d2de56ed796a151be9b6af163b833a070
fc311 <formcollect311@gmail.com>
2026-01-29 15:05:57 +0530

    fizzbuzz array generation

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,15 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+
+  const retArr = []
+
+  for (let count = 1; count <= num; count++) {
+    if (count % 3 == 0 && count % 5 == 0) retArr.push("FizzBuzz")
+    else if (count % 3 == 0 && count % 5 != 0) retArr.push("Fizz")
+    else if (count % 3 != 0 && count % 5 == 0) retArr.push("Buzz")
+    else retArr.push(count)
+  }
+
+  return retArr
+}
 
 module.exports = fizzBuzzArray;
