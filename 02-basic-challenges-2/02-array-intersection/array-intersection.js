a6cdff65dc07c33b3a3d2ebd7e1b3467ebbbf6b2
fc311 <formcollect311@gmail.com>
2026-01-29 15:13:13 +0530

    find arrays' intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+
+  const retArr = []
+
+  for (const ele of arr1) {
+    if (arr2.includes(ele) && !retArr.includes(ele)) retArr.push(ele)
+  }
+
+  return retArr
+}
 
 module.exports = arrayIntersection;
