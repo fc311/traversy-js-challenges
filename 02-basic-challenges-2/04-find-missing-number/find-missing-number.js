48e2dc7bdaeda108f61d717f92bfb41a978ba107
fc311 <formcollect311@gmail.com>
2026-01-30 16:24:23 +0530

    find missing number and missing letter

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,16 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+
+  if (arr.length === 0) return 1
+  if (!arr) return undefined
+
+  arr.sort((l, m) => l - m)
+
+  const n = arr[arr.length - 1]
+
+  for (let num = 1; num <= n; num++) {
+    if (!arr.includes(num)) return num
+  }
+
+}
 
 module.exports = findMissingNumber;
--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,17 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+
+  const firstCharCode = arr[0].charCodeAt(0)
+
+  for (let count = 1; count < arr.length; count++) {
+    const expectedCharCode = firstCharCode + count
+
+    const actualCharCode = arr[count].charCodeAt(0)
+
+    if (expectedCharCode !== actualCharCode) {
+      return String.fromCharCode(expectedCharCode)
+    }
+  }
+
+}
 
 module.exports = findMissingLetter;
