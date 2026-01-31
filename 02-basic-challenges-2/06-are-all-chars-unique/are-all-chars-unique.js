dcef40e4069a2dd4f85f347619f1da08e729b1dd
fc311 <formcollect311@gmail.com>
2026-01-31 18:48:35 +0530

    are all char unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,13 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+
+  const charUnique = {}
+
+  for (const char of str) {
+    if (charUnique[char]) return false
+    charUnique[char] = true
+  }
+
+  return true
+}
 
 module.exports = areAllCharactersUnique;
