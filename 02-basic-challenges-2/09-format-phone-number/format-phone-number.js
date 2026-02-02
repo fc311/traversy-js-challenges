1a90e0bcc4854ba7930b56be79b6c52bbfd4c4f3
fc311 <formcollect311@gmail.com>
2026-02-02 19:05:49 +0530

    format phone number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,11 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(number) {
+
+  const first = number.slice(0, 3).join("")
+  const middle = number.slice(3, 6).join("")
+  const last = number.slice(6,).join("")
+
+  return `(${first}) ${middle}-${last}`
+
+}
 
 module.exports = formatPhoneNumber;
