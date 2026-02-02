1d4727017cc3db6b3e90231f434caaa17d1ae769
fc311 <formcollect311@gmail.com>
2026-02-02 19:34:18 +0530

    validate email

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,14 @@
-function validateEmail() {}
+function validateEmail(email) {
+
+  const splitAt = email.split('@')
+
+  if (splitAt.length === 2) {
+    if (splitAt[0].length > 0 && splitAt[1].includes(".")) {
+      return true
+    }
+  }
+  return false
+
+}
 
 module.exports = validateEmail;
