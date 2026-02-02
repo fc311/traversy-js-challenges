16ca5ded961a348edbeb4837b0403f45ca066dd3
fc311 <formcollect311@gmail.com>
2026-02-02 16:57:45 +0530

    dice game

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,34 @@
-function diceGameSimulation() {}
+
+const singleRoll = () => {
+  const dice1 = Math.ceil(Math.random() * 5)
+  const dice2 = Math.ceil(Math.random() * 5)
+
+  const sum = dice1 + dice2
+
+  let result
+
+  if (sum === 7 || sum === 11) result = "win"
+  else if (sum === 2 || sum === 3 || sum === 12) result = "lose"
+  else result = "roll again"
+
+  return {
+    dice1,
+    dice2,
+    sum,
+    result
+  }
+}
+
+
+function diceGameSimulation(numSimulations) {
+
+  const retArr = []
+
+  for (let count = 0; count < numSimulations; count++) {
+    retArr.push(singleRoll())
+  }
+
+  return retArr
+}
 
 module.exports = diceGameSimulation;
