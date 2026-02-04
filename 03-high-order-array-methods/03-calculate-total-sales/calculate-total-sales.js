1dd6e39748d47452cd0ec8d7a66325e578edc8b8
fc311 <formcollect311@gmail.com>
2026-02-04 16:37:00 +0530

    calculate total sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,11 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+
+  const totalForProducts = products.reduce((total, item) => {
+    return total + (item.price * item.quantity)
+  }, 0)
+  const taxAmount = totalForProducts * (taxRate / 100)
+  return totalForProducts + taxAmount
+
+}
 
 module.exports = calculateTotalSalesWithTax;
