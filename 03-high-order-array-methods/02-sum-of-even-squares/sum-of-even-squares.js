697b3ff85725e6b67499d38e20179d146b647371
fc311 <formcollect311@gmail.com>
2026-02-04 16:18:30 +0530

    sum of even squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,13 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+
+  const final = numbers.reduce((acc, item) => {
+    if (item % 2 === 0) {
+      return acc + (item * item)
+    }
+    return acc + 0
+  }, 0)
+
+  return final
+}
 
 module.exports = sumOfEvenSquares;
