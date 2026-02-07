cd8ec3c09a8202dec8009d8118672b3667c50445
fc311 <formcollect311@gmail.com>
2026-02-07 18:41:37 +0530

    highest scoring word

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,32 @@
-function highestScoringWord() {}
+const wordToScore = (word) => {
+  const score = word
+    .split('')
+    .map(char => char.charCodeAt(0))
+    .reduce((sum, points) => sum + (points - 96), 0)
+
+  return score
+}
+
+function highestScoringWord(str) {
+
+  const words = str.split(" ")
+  const scores = words.map(wordToScore)
+
+  // let highestScoringIndex = 0
+
+  // for (let count = 1; count < scores.length; count++) {
+  //   if (scores[count] > scores[highestScoringIndex]) {
+  //     highestScoringIndex = count
+  //   }
+  // }
+
+  // return words[highestScoringIndex]
+
+  const highestScore = Math.max(...scores)
+  const highestIndex = scores.indexOf(highestScore)
+
+  return words[highestIndex]
+
+}
 
 module.exports = highestScoringWord;
