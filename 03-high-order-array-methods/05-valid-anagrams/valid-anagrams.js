64c3656fe3a4e2df424274244b05fef35931758b
fc311 <formcollect311@gmail.com>
2026-02-24 17:40:22 +0530

    valid anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,10 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+
+  const sortedString1 = str1.split("").sort().join("")
+  const sortedString2 = str2.split("").sort().join("")
+
+  return sortedString1 === sortedString2
+
+}
 
 module.exports = validAnagrams;
